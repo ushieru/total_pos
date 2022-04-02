3600580ddf1d19271e4567534a8b0430a2ee2f49
ushieru <uzielcocolan@gmail.com>
2022-04-02 17:21:27 -0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/total_pos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.total_pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:total_pos/app/total_pos.dart';
+
+void main() {
+  runApp(const TotalPos());
+}
