db5b16a5fd3e633573938b0b993cc3cd04e62d59
ushieru <uzielcocolan@gmail.com>
2022-04-02 17:22:55 -0600

    add admin view

---
+++ lib/app/pages/admin/admin.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:total_pos/app/pages/admin/cubit/admin_cubit.dart';
+import 'package:total_pos/app/pages/admin/screens/categories/category_admin_screen.dart';
+import 'package:total_pos/app/pages/admin/screens/products/product_admin_screen.dart';
+import 'package:total_pos/app/pages/admin/screens/users/user_admin_screen.dart';
+
+class Admin extends StatelessWidget {
+  static const String routeName = '/Admin';
+  const Admin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => AdminCubit(),
+      child: const _AdminView(),
+    );
+  }
+}
+
+class _AdminView extends StatelessWidget {
+  const _AdminView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(
+            width: 60,
+            height: double.maxFinite,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(255, 194, 193, 193),
+                  blurRadius: 10,
+                  spreadRadius: .5,
+                  offset: Offset(1, 0),
+                ),
+              ],
+            ),
+            child: BlocBuilder<AdminCubit, AdminState>(
+              builder: (context, state) {
+                _buildIconButton(int position, String label, IconData icon,
+                    Widget container) {
+                  final isSelected = state.sideBarItemselected == position;
+                  return Column(
+                    children: [
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        color: isSelected ? Colors.purple : Colors.black,
+                        onPressed: () => context
+                            .read<AdminCubit>()
+                            .setSideBarItemAndContainer(position, container),
+                        icon: Icon(icon),
+                      ),
+                      Text(
+                        label,
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: isSelected ? Colors.purple : Colors.black),
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  );
+                }
+
+                return Column(
+                  children: [
+                    const SizedBox(height: 100),
+                    _buildIconButton(
+                      0,
+                      'Home',
+                      Icons.home_rounded,
+                      const SizedBox(),
+                    ),
+                    _buildIconButton(
+                      1,
+                      'Categorias',
+                      Icons.category_rounded,
+                      const CategoryAdminScreen(),
+                    ),
+                    _buildIconButton(
+                      2,
+                      'Productos',
+                      Icons.shopping_bag_rounded,
+                      const ProductAdminScreen(),
+                    ),
+                    _buildIconButton(
+                      3,
+                      'Usuarios',
+                      Icons.account_circle_rounded,
+                      const UserAdminScreen(),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(50),
+              height: double.maxFinite,
+              child: BlocBuilder<AdminCubit, AdminState>(
+                builder: (context, state) {
+                  return state.container;
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
