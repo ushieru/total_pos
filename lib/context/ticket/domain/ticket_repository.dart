0c97540364babcb381cd0dc54152a3e92be80578
ushieru <uzielcocolan@gmail.com>
2022-04-05 13:06:57 -0500

    Fix create Ticket

--- lib/context/ticket/domain/ticket_repository.dart
+++ lib/context/ticket/domain/ticket_repository.dart
@@ -3,6 +3,33 @@ import 'package:total_pos/context/ticket/domain/ticket.dart';
 import 'package:total_pos/context/ticket/domain/ticket_product.dart';
 
 abstract class TicketRepository implements GenericCrudRepository<Ticket> {
-  Future<Ticket> addTicketProduct(Ticket ticket, TicketProduct ticketProduct);
-  Future<Ticket> removeTicketProduct(Ticket ticket, TicketProduct ticketProduct);
+  Future<Ticket> addTicketProduct(
+      Ticket ticket, TicketProduct ticketProduct) async {
+    final isTicketProductInTicket = ticket.ticketProducts
+        .any((_ticketProduct) => _ticketProduct.id == ticketProduct.id);
+    if (isTicketProductInTicket) {
+      final ticketProducts = ticket.ticketProducts
+          .map((_ticketProduct) => _ticketProduct.id == ticketProduct.id
+              ? TicketProduct(ticketProduct.product,
+                  quantity: _ticketProduct.quantity + 1)
+              : _ticketProduct)
+          .toList();
+      return Ticket(ticketProducts, id: ticket.id);
+    }
+    return Ticket([...ticket.ticketProducts, ticketProduct], id: ticket.id);
+  }
+
+  Future<Ticket> removeTicketProduct(
+      Ticket ticket, TicketProduct ticketProduct) async {
+    ticket.ticketProducts
+        .firstWhere((_ticketProduct) => _ticketProduct.id == ticketProduct.id);
+    final ticketProducts = ticket.ticketProducts
+        .map((_ticketProduct) => _ticketProduct.id == ticketProduct.id
+            ? TicketProduct(ticketProduct.product,
+                quantity: _ticketProduct.quantity - 1)
+            : _ticketProduct)
+        .where((_ticketProduct) => _ticketProduct.quantity > 0)
+        .toList();
+    return Ticket(ticketProducts, id: ticket.id);
+  }
 }
--- lib/context/ticket/infrastructure/persistence/in_memory/ticket_in_memory.dart
+++ lib/context/ticket/infrastructure/persistence/in_memory/ticket_in_memory.dart
@@ -1,5 +1,4 @@
 import 'package:total_pos/context/ticket/domain/ticket.dart';
-import 'package:total_pos/context/ticket/domain/ticket_product.dart';
 import 'package:total_pos/context/ticket/domain/ticket_repository.dart';
 
 class TicketInMemory extends TicketRepository {
@@ -13,17 +12,14 @@ class TicketInMemory extends TicketRepository {
   @override
   Future<Ticket> getByID(String id) async {
     final product = _tickets[id];
-    if (product == null) {
-      throw '<TicketInMemory> Product not found';
-    }
+    if (product == null) throw '<TicketInMemory> Product not found';
     return product;
   }
 
   @override
   Future<Ticket> create(Ticket object) async {
-    if (_tickets.containsKey(object.id)) {
-      throw '<TicketInMemory> ID duplicate';
-    }
+    if (object.total == 0) throw '<TicketInMemory> Ticket is empty';
+    if (_tickets.containsKey(object.id)) throw '<TicketInMemory> ID duplicate';
     _tickets[object.id] = object;
     return object;
   }
@@ -41,57 +37,5 @@ class TicketInMemory extends TicketRepository {
     }
     _tickets[object.id] = object;
     return object;
-  }
-
-  @override
-  Future<Ticket> addTicketProduct(
-      Ticket ticket, TicketProduct ticketProduct) async {
-    if (!_tickets.containsKey(ticket.id)) {
-      final newTicket = Ticket([ticketProduct], id: ticket.id);
-      _tickets[ticket.id] = newTicket;
-      return newTicket;
-    }
-    final oldTicket = _tickets[ticket.id]!;
-    try {
-      oldTicket.ticketProducts.firstWhere(
-          (oldTicketProduct) => oldTicketProduct.id == ticketProduct.id);
-
-      final ticketProducts = oldTicket.ticketProducts
-          .map((oldTicketProduct) => oldTicketProduct.id == ticketProduct.id
-              ? TicketProduct(ticketProduct.product,
-                  quantity: oldTicketProduct.quantity + 1)
-              : oldTicketProduct)
-          .toList();
-      final newTicket = Ticket(ticketProducts, id: oldTicket.id);
-      _tickets[ticket.id] = newTicket;
-      return newTicket;
-    } catch (e) {
-      final newTicket = Ticket([...oldTicket.ticketProducts, ticketProduct],
-          id: oldTicket.id);
-      _tickets[ticket.id] = newTicket;
-      return newTicket;
-    }
-  }
-
-  @override
-  Future<Ticket> removeTicketProduct(
-      Ticket ticket, TicketProduct ticketProduct) async {
-    if (!_tickets.containsKey(ticket.id)) {
-      throw '<TicketInMemory> Ticket not found';
-    }
-    final oldTicket = _tickets[ticket.id]!;
-    oldTicket.ticketProducts.firstWhere(
-        (oldTicketProduct) => oldTicketProduct.id == ticketProduct.id);
-
-    final ticketProducts = oldTicket.ticketProducts
-        .map((oldTicketProduct) => oldTicketProduct.id == ticketProduct.id
-            ? TicketProduct(ticketProduct.product,
-                quantity: oldTicketProduct.quantity - 1)
-            : oldTicketProduct)
-        .where((oldTicketProduct) => oldTicketProduct.quantity > 0)
-        .toList();
-    final newTicket = Ticket(ticketProducts, id: oldTicket.id);
-    _tickets[ticket.id] = newTicket;
-    return newTicket;
   }
 }
