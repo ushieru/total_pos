6aa2e7de23251ffb82536a145d7b681955004ef7
ushieru <uzielcocolan@gmail.com>
2022-04-06 23:28:29 -0500

    delete application folder

--- lib/context/shared/application/delete.dart
+++
@@ -1,7 +0,0 @@
-import 'package:total_pos/context/shared/domain/generic_crud_repository.dart';
-
-class Delete<T> {
-  final GenericCrudRepository<T> _repository;
-  Delete(this._repository);
-  Future<void> run(T object) => _repository.delete(object);
-}
--- lib/context/shared/application/get_by_id.dart
+++
@@ -1,7 +0,0 @@
-import 'package:total_pos/context/shared/domain/generic_crud_repository.dart';
-
-class GetByID<T> {
-  final GenericCrudRepository<T> _repository;
-  GetByID(this._repository);
-  Future<T> run(String id) => _repository.getByID(id);
-}
