99efee8780243c75c1e03abbe99e756f61d18d24
ushieru <uzielcocolan@gmail.com>
2022-04-07 15:31:36 -0500

    add private page

--- lib/app/total_pos.dart
+++ lib/app/total_pos.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:total_pos/app/config/global_config.dart';
+import 'package:total_pos/app/cubit/session_cubit.dart';
 import 'package:total_pos/app/lib/setup_locator.dart';
 import 'package:total_pos/app/pages/admin/admin.dart';
 import 'package:total_pos/app/pages/cashier/cashier.dart';
 import 'package:total_pos/app/pages/loading/loading.dart';
 import 'package:total_pos/app/pages/login/login.dart';
+import 'package:total_pos/app/widgets/private_page.dart';
 
 class TotalPos extends StatelessWidget {
   TotalPos({Key? key}) : super(key: key) {
@@ -13,17 +16,20 @@ class TotalPos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Total Pos',
-      theme: ThemeData(primarySwatch: GlobalConfig.principalColor),
-      routes: {
-        Loading.routeName: (context) => const Loading(),
-        Login.routeName: (context) => const Login(),
-        Admin.routeName: (context) => const Admin(),
-        Cashier.routeName: (context) => const Cashier(),
-      },
-      initialRoute: Loading.routeName,
-      debugShowCheckedModeBanner: false,
+    return BlocProvider(
+      create: (context) => SessionCubit(),
+      child: MaterialApp(
+        title: 'Total Pos',
+        theme: ThemeData(primarySwatch: GlobalConfig.principalColor),
+        routes: {
+          Loading.routeName: (context) => const Loading(),
+          Login.routeName: (context) => const Login(),
+          Admin.routeName: (context) => const PrivatePage(route: Admin()),
+          Cashier.routeName: (context) => const PrivatePage(route: Cashier()),
+        },
+        initialRoute: Loading.routeName,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
