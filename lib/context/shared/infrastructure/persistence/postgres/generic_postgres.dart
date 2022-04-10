806cc076286943d34990c986be9a621d65b93ec7
ushieru <uzielcocolan@gmail.com>
2022-04-10 17:10:45 -0500

    add query debugger

--- lib/context/shared/infrastructure/persistence/postgres/generic_postgres.dart
+++ lib/context/shared/infrastructure/persistence/postgres/generic_postgres.dart
@@ -4,7 +4,7 @@ import 'package:total_pos/context/shared/domain/serializable.dart';
 import 'package:total_pos/context/shared/infrastructure/builder/postgres_query_builder.dart';
 import 'package:total_pos/context/shared/infrastructure/persistence/postgres/postgres_connection.dart';
 
-class GenericPostgres<T extends Serializable>
+abstract class GenericPostgres<T extends Serializable>
     implements GenericCrudRepository<T> {
   final pConnection = PostgresConnection();
   final queryBuilderRepository = PostgresQueryBuilder();
@@ -12,43 +12,54 @@ class GenericPostgres<T extends Serializable>
   @override
   Future<T> create(T object) async {
     final connection = await pConnection.getConnection();
-    await connection.query(queryBuilderRepository.insert(object));
+    final query = queryBuilderRepository.insert(object);
+    debugQueryPrinter(query);
+    await connection.query(query);
     return object;
   }
 
   @override
   Future<void> delete(T object) async {
     final connection = await pConnection.getConnection();
-    await connection.query(queryBuilderRepository.delete(object));
+    final query = queryBuilderRepository.delete(object);
+    debugQueryPrinter(query);
+    await connection.query(query);
   }
 
   @override
   Future<List<T>> getAll() async {
     final connection = await pConnection.getConnection();
-    final result = await connection.query(queryBuilderRepository.findAll<T>());
+    final query = queryBuilderRepository.findAll<T>();
+    debugQueryPrinter(query);
+    final result = await connection.query(query);
     return genericToPostgreSQLResult(result);
   }
 
   @override
   Future<T> getByID(String id) async {
     final connection = await pConnection.getConnection();
-    final result =
-        await connection.query(queryBuilderRepository.findById<T>(id));
+    final query = queryBuilderRepository.findById<T>(id);
+    debugQueryPrinter(query);
+    final result = await connection.query(query);
     return genericToPostgreSQLResult(result).first;
   }
 
   @override
   Future<T> update(T object) async {
     final connection = await pConnection.getConnection();
-    connection.query(queryBuilderRepository.update(object));
+    final query = queryBuilderRepository.update(object);
+    debugQueryPrinter(query);
+    connection.query(query);
     return object;
   }
 
-  T genericToPostgreSQLResultRow(PostgreSQLResultRow row) {
-    throw 'Override this method';
-  }
+  T genericToPostgreSQLResultRow(PostgreSQLResultRow row);
 
   List<T> genericToPostgreSQLResult(PostgreSQLResult result) {
     return result.map((row) => genericToPostgreSQLResultRow(row)).toList();
+  }
+
+  void debugQueryPrinter(String query) {
+    print('<GenericPostgres extends ${T.runtimeType}> query: $query');
   }
 }
