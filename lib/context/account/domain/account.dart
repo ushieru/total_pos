6145d36a9bb9498a3c08f4e16ccdf6196f9dc164
ushieru <uzielcocolan@gmail.com>
2022-04-10 17:09:25 -0500

    add serializable

--- lib/context/account/domain/account.dart
+++ lib/context/account/domain/account.dart
@@ -1,13 +1,18 @@
+import 'package:total_pos/context/shared/domain/serializable.dart';
 import 'package:uuid/uuid.dart';
 
-class Account {
+class Account implements Serializable {
   final String id;
   final String password;
   final String user;
-
   Account(this.password, this.user, {String? id})
       : id = id ?? const Uuid().v4();
-
   @override
   String toString() => 'Account(id: $id, password: $password, user: $user)';
+  @override
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'password': password,
+        'user': user,
+      };
 }
