4fb8206021c8d9df12e08845a4a1db855b95de7f
ushieru <uzielcocolan@gmail.com>
2022-05-07 14:58:06 -0500

    Add dump sql

---
+++ lib/backend/assets/postgres/dump_db.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS public."account" (
+    "id" character varying COLLATE pg_catalog."default" NOT NULL,
+    "password" character varying COLLATE pg_catalog."default" NOT NULL,
+    "user" character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT account_pkey PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS public."user" (
+    "id" character varying COLLATE pg_catalog."default" NOT NULL,
+    "name" character varying COLLATE pg_catalog."default" NOT NULL,
+    "email" character varying COLLATE pg_catalog."default" NOT NULL,
+    "role" character varying COLLATE pg_catalog."default" NOT NULL,
+    "accountId" character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT user_pkey PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS public."category" (
+    "id" character varying COLLATE pg_catalog."default" NOT NULL,
+    "name" character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT category_pkey PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS public."product" (
+    "id" character varying COLLATE pg_catalog."default" NOT NULL,
+    "name" character varying COLLATE pg_catalog."default" NOT NULL,
+    "description" character varying COLLATE pg_catalog."default" NOT NULL,
+    "price" numeric NOT NULL,
+    "categoryId" character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT product_pkey PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS public."ticket" (
+    "id" character varying COLLATE pg_catalog."default" NOT NULL,
+    "userId" character varying COLLATE pg_catalog."default" NOT NULL,
+    "datetime" bigint NOT NULL,
+    "products" json NOT NULL,
+    "total" numeric NOT NULL,
+    CONSTRAINT ticket_pkey PRIMARY KEY (id)
+);
