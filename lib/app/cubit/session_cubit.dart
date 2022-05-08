bf7896d96430b3dbc48105e00ddeee259f96a97a
ushieru <uzielcocolan@gmail.com>
2022-05-08 17:02:25 -0500

    Fix session cubit controller

--- lib/app/cubit/session_cubit.dart
+++ lib/app/cubit/session_cubit.dart
@@ -4,9 +4,11 @@ import 'package:total_pos/context/user/domain/user.dart';
 
 part 'session_state.dart';
 
-class SessionCubit extends Cubit<User?> {
-  SessionCubit() : super(null);
+class SessionCubit extends Cubit<SessionInitial> {
+  SessionCubit() : super(const SessionInitial(null));
 
-  void setSession(User user) => emit(user);
-  void deleteSession() => emit(null);
+  void setSession(User user) => emit(SessionInitial(user));
+
+  void deleteSession({bool redirect = true}) =>
+      emit(SessionInitial(null, redirect: redirect));
 }
--- lib/app/cubit/session_state.dart
+++ lib/app/cubit/session_state.dart
@@ -1,6 +1,13 @@
 part of 'session_cubit.dart';
 
 @immutable
-abstract class SessionState {}
+abstract class SessionState {
+  final User? user;
+  final bool redirect;
+  const SessionState(this.user, this.redirect);
+}
 
-class SessionInitial extends SessionState {}
+class SessionInitial extends SessionState {
+  const SessionInitial(User? user, {bool redirect = true})
+      : super(user, redirect);
+}
